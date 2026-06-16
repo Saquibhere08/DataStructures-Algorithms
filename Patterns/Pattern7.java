70a3fadcc70264e2430fceecf39d6e00b066e428
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-06-16 19:02:47 +0530

    Add files via upload

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,17 @@
+package patterns;
+public class Pattern7 {
+	public static void main(String[] args) {
+		
+	}
+}
+/*
+ 7.Given an integer n. 
+ You need to recreate the pattern given below for any value of N.
+Let's say for N = 5, the pattern should look like as below:
+
+    *
+   ***
+  *****
+ *******
+*********
+ */
